e351bcbfcd9ff2d386af58a479a168b8fe0aa0c5
LauraAlessioo <laura.carvalho@sptech.school>
2025-08-13 20:26:43 -0300

    Evoluindo Home

---
+++ AEJLivrosFront/src/componentes/Acessibilidade.jsx
@@ -0,0 +1,11 @@
+import '../StyleAej.css'
+
+export function Acessibilidade(){
+    return(
+        <div>
+            <div className="mainCircle">
+                <i class='bx bx-body'></i>
+            </div>
+        </div>
+    )
+}
--- AEJLivrosFront/src/pages/Home.jsx
+++ AEJLivrosFront/src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { Acessibilidade } from "../componentes/Acessibilidade";
 import { Header } from "../componentes/Header";
 
 export default function Home() {
     return (
         <div>
+            <Acessibilidade/>
+            <session className="primeira">
+
             <Header/>
+            </session>
+            <session className="segunda">
+                <div className="recomend">
+                    <h2>Recomendações</h2>
+                    <div className="recSpace"></div>
+                </div>
+                <div className="cat">
+                    <h2>Categorias</h2>
+                    <div className="catSpace"></div>
+                </div>
+            </session>
         </div>
     );
 }
