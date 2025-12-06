9984cb60d214eeb3f2a6079b465fccc726bbd7ab
Felipe Adelungue Gasparotto <felipe.gasparotto@sptech.school>
2025-12-05 21:20:09 -0300

    Removed comments at docker-entrypoint.sh

--- AEJLivrosFront/docker-entrypoint.sh
+++ AEJLivrosFront/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# Substitui ${API_URL} pela variável de ambiente real
 envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-# Inicia NGINX
 exec nginx -g 'daemon off;'
