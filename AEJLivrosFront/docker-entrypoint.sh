e9ebe1ba9c78973983849cc2cad669460673859b
leoderaldi <leoderaldi@gmail.com>
2025-12-07 01:04:27 -0300

    fix(docker): adicionar valor padrão para API_URL no entrypoint

--- AEJLivrosFront/docker-entrypoint.sh
+++ AEJLivrosFront/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Define valor padrão para API_URL se não estiver definida
+export API_URL="${API_URL:-http://localhost:8080}"
+
 envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
